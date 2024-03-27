426b9e97a16a150895b3b95139e4b2e8caf97a1b
celinec1 <cchen@bu.edu>
2024-03-27 14:39:00 -0400

    added Hello application

---
+++ celine-hello/src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          :)
+        </h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Hello World!
+          I'm Celine Chen
+        </p>
+        <a
+          className="App-link"
+          href="https:linkedin.com/in/cceline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn about Me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
